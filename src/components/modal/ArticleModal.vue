0db38c993f8cd46524a3793d7deef78d101a8f72
rockayumitw <rockayumitw@gmail.com>
2021-07-02 14:26:05 +0800

    update

--- src/components/modal/ArticleModal.vue
+++ src/components/modal/ArticleModal.vue
@@ -7,7 +7,6 @@
     aria-labelledby="exampleModalLabel"
     aria-hidden="true"
     ref="articleModal"
-    @hide="test"
   >
     <div class="modal-dialog modal-xl" role="document">
       <div class="modal-content border-0">
@@ -24,16 +23,21 @@
           ></button>
         </div>
         <div class="modal-body">
-          <div class="row">
-            <div class="col-sm-4">
+          <!--表單-->
+          <form class="row needs-validation" novalidate>
+            <div class="col-md-4">
               <div class="mb-3">
-                <label for="title" class="form-label">標題</label>
+                <label for="title" class="form-label">
+                  <small class="text-danger">*</small>
+                  標題
+                </label>
                 <input
                   type="text"
                   class="form-control"
                   id="title"
                   v-model="tempArticle.title"
                   placeholder="請輸入標題"
+                  required
                 />
               </div>
               <div class="mb-3">
@@ -47,30 +51,38 @@
                 />
               </div>
               <div class="mb-3">
-                <label for="author" class="form-label">作者</label>
+                <label for="author" class="form-label">
+                  <small class="text-danger">*</small>
+                  作者
+                </label>
                 <input
                   type="text"
                   class="form-control"
                   id="author"
                   v-model="tempArticle.author"
                   placeholder="請輸入標題"
+                  required
                 />
               </div>
               <div class="mb-3">
-                <label for="create_at">文章建立日期</label>
+                <label for="create_at">
+                  <small class="text-danger">*</small>
+                  文章建立日期
+                </label>
                 <input
                   type="date"
                   class="form-control"
                   id="create_at"
                   v-model="create_at"
+                  required
                 />
               </div>
             </div>
-            <div class="col-sm-8">
+            <div class="col-md-8">
               <label for="tag" class="form-label">標籤</label>
               <div class="row gx-1 mb-3">
                 <div
-                  class="col-md-2 mb-1"
+                  class="col-md-3 mb-1"
                   v-for="(label, key) in tempArticle.tag"
                   :key="key"
                 >
@@ -104,12 +116,16 @@
                   </button>
                 </div>
               </div>
-              <label for="content" class="form-label">文章描述</label>
+              <label for="content" class="form-label">
+                <small class="text-danger">*</small>
+                文章描述
+              </label>
               <div class="mb-3">
                 <ckeditor
                   :editor="editor"
                   :config="editorConfig"
                   v-model="tempArticle.description"
+                  required
                 ></ckeditor>
               </div>
               <div class="mb-3">
@@ -119,31 +135,32 @@
                     type="checkbox"
                     v-model="tempArticle.isPublic"
                     id="isPublic"
+                    required
                   />
                   <label class="form-check-label" for="isPublic">
+                    <small class="text-danger">*</small>
                     是否公開
                   </label>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-gray-100"
-            data-bs-dismiss="modal"
-          >
-            取消
-          </button>
-          <button
-            type="button"
-            class="btn btn-secondary text-white"
-            data-bs-dismiss="modal"
-            @click="$emit('update-article', tempArticle)"
-          >
-            確認
-          </button>
+            <div class="d-flex justify-content-end">
+              <button
+                type="button"
+                class="btn btn-gray-100 mr-2"
+                data-bs-dismiss="modal"
+              >
+                取消
+              </button>
+              <button
+                type="submit"
+                class="btn btn-secondary text-white"
+                @click="updateArticle()"
+              >
+                確認
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -151,13 +168,16 @@
 </template>
 <script>
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Modal from 'bootstrap/js/dist/modal';
 
 export default {
   props: {
     article: {
       type: Object,
       default() {
-        return {};
+        return {
+          articleModal: '',
+        };
       },
     },
   },
@@ -190,12 +210,62 @@ export default {
     },
   },
   mounted() {
+    this.articleModal = new Modal(this.$refs.articleModal, {
+      keyboard: false,
+      backdrop: 'static',
+    });
     this.$refs.articleModal.addEventListener('hidden.bs.modal', () => {
+      this.init();
+    });
+  },
+  methods: {
+    init() {
       this.tempArticle = {
         tag: [''],
       };
       this.create_at = 0;
-    });
+      const forms = document.querySelector('.needs-validation');
+      forms.classList.remove('was-validated');
+    },
+    modal(state) {
+      switch (state) {
+        case 'show':
+          this.articleModal.show();
+          break;
+        case 'hide':
+          this.articleModal.hide();
+          break;
+        default:
+      }
+    },
+    updateArticle() {
+      let check = false; // 未通過
+      const forms = document.querySelectorAll('.needs-validation');
+      console.log(forms);
+      Array.prototype.slice.call(forms).forEach((form) => {
+        console.log(form);
+        form.addEventListener('submit', (event) => {
+          console.log(event);
+          console.log(form.checkValidity());
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+            check = false;
+          } else {
+            check = true;
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+      // 非同步
+      setTimeout(() => {
+        if (check) {
+          this.modal('hide');
+          this.$emit('update-article', this.tempArticle);
+          this.init();
+        }
+      });
+    },
   },
 };
 </script>
--- src/components/modal/CouponModal.vue
+++ src/components/modal/CouponModal.vue
@@ -14,50 +14,69 @@
             data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <div class="mb-3">
-            <label for="title">標題</label>
-            <input type="text" class="form-control" id="title" v-model="tempCoupon.title"
-                   placeholder="請輸入標題">
+          <!--表單-->
+          <form class="needs-validation" novalidate>
+            <div class="mb-3">
+              <label for="title">
+                <small class="text-danger">*</small>
+                標題
+              </label>
+              <input type="text" class="form-control"
+              id="title"
+              v-model="tempCoupon.title"
+              placeholder="請輸入標題" required>
           </div>
           <div class="mb-3">
-            <label for="coupon_code">優惠碼</label>
-            <input type="text" class="form-control" id="coupon_code" v-model="tempCoupon.code"
-                   placeholder="請輸入優惠碼">
+            <label for="coupon_code">
+              <small class="text-danger">*</small>
+              優惠碼
+            </label>
+            <input type="text" class="form-control" id="coupon_code"
+            v-model="tempCoupon.code" placeholder="請輸入優惠碼" required>
           </div>
           <div class="mb-3">
-            <label for="due_date">到期日</label>
-            <input type="date" class="form-control" id="due_date"
-                   v-model="due_date">
+            <label for="due_date">
+              <small class="text-danger">*</small>
+              到期日
+            </label>
+            <input type="date" class="form-control" id="due_date" v-model="due_date" required>
           </div>
           <div class="mb-3">
-            <label for="price">折扣百分比</label>
+            <label for="price">
+              <small class="text-danger">*</small>
+              折扣百分比
+            </label>
             <input type="number" class="form-control" id="price"
-            min="0"
-                   v-model.number="tempCoupon.percent" placeholder="請輸入折扣百分比">
+            min="0" v-model.number="tempCoupon.percent" placeholder="請輸入折扣百分比" required>
           </div>
           <div class="mb-3">
             <div class="form-check">
               <input class="form-check-input" type="checkbox"
-                     :true-value="1"
-                     :false-value="0"
-                     v-model="tempCoupon.is_enabled" id="is_enabled">
+                :true-value="1"
+                :false-value="0"
+                v-model="tempCoupon.is_enabled" id="is_enabled" required>
               <label class="form-check-label" for="is_enabled">
+                <small class="text-danger">*</small>
                 是否啟用
               </label>
             </div>
           </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-gray-100" data-bs-dismiss="modal">取消</button>
-          <button type="button" class="btn btn-secondary text-white" data-bs-dismiss="modal"
-                  @click="$emit('update-coupon', tempCoupon), init()">確定
-          </button>
+          <div class="d-flex justify-content-end">
+            <button type="button" class="btn btn-gray-100 mr-2"
+            data-bs-dismiss="modal">取消</button>
+            <!--data-bs-dismiss="modal"-->
+            <button type="submit" class="btn btn-secondary text-white"
+              @click="updateCoupon">確定
+            </button>
+          </div>
+          </form>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import Modal from 'bootstrap/js/dist/modal';
 
 export default {
   props: {
@@ -72,18 +91,8 @@ export default {
     return {
       tempCoupon: {},
       due_date: '',
+      couponModal: '',
     };
-  },
-  mounted() {
-    this.$refs.couponModal.addEventListener('hidden.bs.modal', () => {
-      this.tempCoupon = {};
-      this.due_date = 0;
-    });
-  },
-  methods: {
-    init() {
-      this.tempCoupon = {};
-    },
   },
   watch: {
     coupon() {
@@ -100,5 +109,60 @@ export default {
     //   [this.due_date] = dateAndTime;
     // },
   },
+  mounted() {
+    this.couponModal = new Modal(this.$refs.couponModal, {
+      keyboard: false,
+      backdrop: 'static',
+    });
+    this.$refs.couponModal.addEventListener('hidden.bs.modal', () => {
+      this.init();
+    });
+  },
+  methods: {
+    init() {
+      this.tempCoupon = {};
+      this.due_date = 0;
+      const forms = document.querySelector('.needs-validation');
+      forms.classList.remove('was-validated');
+    },
+    modal(state) {
+      switch (state) {
+        case 'show':
+          this.couponModal.show();
+          break;
+        case 'hide':
+          this.couponModal.hide();
+          break;
+        default:
+      }
+    },
+    updateCoupon() {
+      // 判斷輸入
+      let check = false; // 未通過
+      const forms = document.querySelectorAll('.needs-validation');
+      Array.prototype.slice.call(forms).forEach((form) => {
+        form.addEventListener('submit', (event) => {
+          console.log(form.checkValidity());
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+            check = false;
+          } else {
+            check = true;
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+      // 非同步
+      setTimeout(() => {
+        if (check) {
+          this.modal('hide');
+          this.$emit('update-coupon', this.tempCoupon);
+          this.init();
+        }
+      });
+    },
+  },
+
 };
 </script>
